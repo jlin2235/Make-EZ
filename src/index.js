218fb2d281bbbe8cd1a2fd92089b0adbd69e6862
Jlin2235 <Jlin2235@gmail.com>
2020-08-19 17:42:03 -0400

    api working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import "./styles/index.scss";
 import "./styles/navbar.scss";
 import "./styles/search.scss";
 import { navbarExpand } from './scripts/navbar'
+import { testSearch } from './scripts/search'
 
 
 document.addEventListener('DOMContentLoaded', () => {
     navbarExpand()
+    testSearch()
 
 
-    
 
 })
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,7 +1,55 @@
+import Axios from 'axios'
 
 
+const getRecipeData = () => {
+    debugger
+    const YOUR_APP_ID = '6f5507b6';
+    const YOUR_APP_KEY = '50592ebb8250b99cec1f597f0426fdcf';
+    const searchValue = document.getElementById('search-bar').value
+    const edamam = `https://api.edamam.com/search?q=${searchValue}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+    
+    const result = Axios.get(edamam)
+    .then(function (res){
+            console.log(res);
+            return res;
+    })
+    .then(res => rendercards(res.data.hits))
+    debugger
+}
 
-const YOUR_APP_ID = '6f5507b6'
-const YOUR_APP_KEY = '50592ebb8250b99cec1f597f0426fdcf'
+const clearValue = () => {
+    document.getElementById('search-bar').value = "";
+}
 
-const edamam = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+
+const rendercards = (data) => {
+    debugger
+    document.getElementById('cards-container').innerHTML = 
+    (data.map(el => {
+        debugger
+        return(
+            `<div id="search-result-container">
+                <img src='${el.recipe.image}'>
+                <h1> <span>Name</span> ${el.recipe.label} </h1>
+                <h1> <span>Calories</span> ${el.recipe.calories} </h1>
+                <form target="_blank" action="${el.recipe.url}">
+                    <input type="submit" value="Go to Recipe" />
+                </form>
+            </div>` 
+        )
+        
+        
+    }))
+            
+}
+
+export const testSearch = () => {
+
+    debugger
+    const searchInput = document.getElementById('search-bar-submit-button')
+    searchInput.addEventListener('click', () => {
+        getRecipeData();
+        clearValue();
+    })
+}
+
